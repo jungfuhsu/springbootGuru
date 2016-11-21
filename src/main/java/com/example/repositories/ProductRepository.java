0c43592319f2497548966e801a46f2f3a94d1d2c
novarse <stephen@novarse.com>
2016-11-22 08:49:58 +1100

    Intial commit. Got H2 database and console running with Spring Boot from https://springframework.guru/spring-boot-web-application-part-4-spring-mvc/

---
+++ src/main/java/com/example/repositories/ProductRepository.java
@@ -0,0 +1,8 @@
+package com.example.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.domain.Product;
+
+public interface ProductRepository extends CrudRepository<Product, Integer> {
+}
